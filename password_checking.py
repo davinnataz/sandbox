ef99bcf664fefaa58f95b4cae1463d597053842f
Davin Natanael <davinnatanael@my.jcu.edu.au>
2021-11-24 11:13:42 +0700

    add password function

--- password_checking.py
+++ password_checking.py
@@ -4,10 +4,11 @@ checking password
 
 def main ():
     password = "ShenXiaoting99"
-    if password == "ShenXiaoting99":
+    if password == getpassword(password):
         print("Access Granted")
     else :
         print("Access Denied")
 
-
+def getpassword(password):
+    password = input("Enter your password : ")
 main()
