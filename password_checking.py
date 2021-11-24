439eb885f06d4dd5a695706d9903bc3284db3283
Davin Natanael <davinnatanael@my.jcu.edu.au>
2021-11-24 11:06:39 +0700

    Initial Commit

---
+++ password_checking.py
@@ -0,0 +1,13 @@
+"""
+checking password
+"""
+
+def main ():
+    password = "ShenXiaoting99"
+    if password == "ShenXiaoting99":
+        print("Access Granted")
+    else :
+        print("Access Denied")
+
+
+main()
