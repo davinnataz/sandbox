7d24f7c0863043bb4978154f983768bc87e22d91
Davin Natanael <davinnatanael@my.jcu.edu.au>
2021-11-24 11:36:46 +0700

    add status function

--- broken_score.py
+++ broken_score.py
@@ -10,5 +10,12 @@ def getscore():
     score =  float(input("Enter your score : "))
 
 def score_status():
-
+    if score >= 50 and score < 90:
+        print("Passable")
+    elif score > 90 and score <= 100:
+        print("Excellent")
+    elif score >= 0 and score < 50:
+        print("Bad")
+    else:
+        print("Invalid Score")
 
